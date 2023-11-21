df8c3c5eb9b2e14694b19f4e26a185e963d687cc
amine <kermouss67@gmail.com>
2023-11-21 12:32:08 +0100

    Comments

--- src/main/java/fsr/ma/Test.java
+++ src/main/java/fsr/ma/Test.java
@@ -2,7 +2,13 @@ package fsr.ma;
 public class Test 
 {
     public static void main (String[] args)
+
     {
+
+        System.out.println ("------------------");
         System.out.println ("Hello !");
     }
+
+    //commentaire
+
 }
